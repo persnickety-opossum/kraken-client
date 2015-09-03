68822cd36cf7df5095c3a45b963fa0570d03645d
Corey Hsu <corey81@gmail.com>
2015-09-02 22:13:17 -0700

    (feat) Add add comment button and style input.

--- app/Venue/venue.index.js
+++ app/Venue/venue.index.js
@@ -2,6 +2,7 @@
 
 var React = require('react-native');
 //var venue = require('./venueMock');
+var Button = require('react-native-button');
 var moment = require('moment');
 moment().format();
 var Display = require('react-native-device-display');
@@ -101,8 +102,46 @@ var VenueTab = React.createClass({
   },
 
   renderComments(comments) {
-    //return <Text>{comments.datetime}: {comments.content}</Text>
-    return <Text>{comments}</Text>
+    return <Text>{comments.datetime}: {comments.content}</Text>
+    //return <Text>{comments}</Text>
+  },
+
+  submitComment() {
+    //this gets called when "submit comment" gets pushed.
+    // {
+//   content: "Comment text",
+//   creator: "55e39290c2b4e82b4839046a", // ID of the user posting the comment
+//   venue: "55e394d6c2b4e82b48390473", // ID of the event that the comment is associated with
+//   datetime: "2016-03-30T06:20:46.000Z",
+//   atVenue: true
+// }
+    var content = this.state.content;
+    var creator = 'fake token for now';
+    var venue = this.state.venue._id;
+    var datetime = new Date().toISOString();
+    var atVenue = true;
+    var postObj = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Origin': ''
+      },
+      body: JSON.stringify({
+        'content': content,
+        'creator': creator,
+        'venue': venue,
+        'datetime': datetime,
+        'atVenue': atVenue
+      })
+    };
+    fetch(config.serverURL + '/api/comments/', postObj)
+      .then(function(res) {
+        return res.json();
+      })
+      //.then(function(resJson) {
+      //  return resJson;
+      //})
   },
 
   render() {
@@ -135,10 +174,13 @@ var VenueTab = React.createClass({
           onValueChange={(voteValue) => this.setState({voteValue: Math.round(voteValue*10)})}
           maximumTrackTintColor='red'/>
         <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          style={styles.textInput}
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}
-          />
+        />
+        <Button style={styles.commentButton} onPress={this.submitComment}>
+          Submit Comment
+        </Button>
         <RefreshableListView
           style={styles.listView}
           dataSource={this.state.dataSource}
@@ -146,7 +188,6 @@ var VenueTab = React.createClass({
           loadData={this.reloadComments}
           refreshDescription="Refreshing comments"
           />
-
 
         <View style={{height: this.state.keyboardSpace}}></View>
       </View>
@@ -186,6 +227,20 @@ var styles = StyleSheet.create({
     marginRight: 40,
     marginBottom: 10,
     flex: 0.5
+  },
+  textInput: {
+    height: 30,
+    borderColor: 'gray',
+    margin: 5,
+    marginBottom: 15,
+    borderWidth: 1
+  },
+  commentButton: {
+    fontSize: 20,
+    flex: 1,
+    textAlign: 'right',
+    right: 10,
+    alignSelf: 'flex-end'
   },
   listView: {
     margin: 10,
