31293b1ac776934e612cc8d85fe71e2a6f47ac6a
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-09-02 18:56:21 -0700

    Add user token to config

---
+++ app/config.example.js
@@ -0,0 +1,5 @@
+// IMPORTANT: Save this file as "config.js" and make the necessary changes below
+// Replace the URL with your server's url
+module.exports.serverURL = 'http://localhost:8000';
+// Replace the token with any unique string (TODO: use device UUIDs instead)
+module.exports.userToken = 'thisIsAToken';
