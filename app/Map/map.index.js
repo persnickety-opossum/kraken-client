d111cc151017de875bbac1aebe0459051e882c1b
Daniel Friedenbach <danielfriedenbach@gmail.com>
2015-09-04 12:10:29 -0700

    (feat) Center initial view on user position

--- app/Map/map.index.js
+++ app/Map/map.index.js
@@ -27,10 +27,6 @@ var MapTab = React.createClass({
   getInitialState() {
     return {
       searchString: '',
-      center: {
-        latitude: 37.783585,
-        longitude: -122.408955
-      },
       zoom: 13,
       venuePins: [],
       searchPins: [],
@@ -96,9 +92,21 @@ var MapTab = React.createClass({
   componentWillMount: function() {
     navigator.geolocation.getCurrentPosition(
       (initialPosition) =>  this.setState({
-        geolocation: initialPosition
+        geolocation: initialPosition,
+        center: {
+          latitude: initialPosition.coords.latitude,
+          longitude: initialPosition.coords.longitude
+        }
       }),
-      (error) => alert(error.message),
+      (error) => {
+        this.setState({
+          center: {
+            latitude: 37.783585,
+            longitude: -122.408955
+          }
+        });
+        alert(error.message);
+      },
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     );
     this.watchID = navigator.geolocation.watchPosition((lastPosition) => {
