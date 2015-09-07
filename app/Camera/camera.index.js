bea18d9bc30ba174c33c9b624f87bf8a3008cf9e
Darryl Blake <darryl@darrylblake.com>
2015-09-07 11:47:03 -0700

    Camera working, saving to server

--- app/Camera/camera.index.js
+++ app/Camera/camera.index.js
@@ -57,70 +57,18 @@ var KrakenCam = React.createClass({
 
   _takePicture() {
     this.refs.cam.capture(function(err, path) {
-
-
-
-      // console.log('path >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', path);
-
-
-      // var obj = {
-      //     uri, // either an 'assets-library' url (for files from photo library) or an image dataURL
-      //     uploadUrl,
-      //     fileName,
-      //     mimeType,
-      //     headers,
-      //     data: {
-      //         // whatever properties you wish to send in the request
-      //         // along with the uploaded file
-      //     }
-      // };
-      // NativeModules.FileTransfer.upload(obj, (err, res) => {
-      //     // handle response
-      //     // it is an object with 'status' and 'data' properties
-      //     // if the file path protocol is not supported the status will be 0
-      //     // and the request won't be made at all
-      // });
-
-
-
-      //CameraRoll.getPhotos(fetchParams, this.storeImages, this.logImageError);
-      // console.log(path, '<<<<<<<<<<<<<<<<<<<<<<<<< PATH')
-      // NativeModules.ReadImageData.readImage(path, (image) => {
-      //     console.log(image);
-      //    fetch('http://10.1.10.39:5000/api/media/venue/', {
-      //       method: 'POST',
-      //       headers: {
-      //         'Accept': 'application/json',
-      //         'Content-Type': 'application/json'
-      //       },
-      //       body: JSON.stringify({photo: image}),
-      //     });
-      // });
-
-      // RNFS.readFile(path)
-      //   .then(function(content) {
-
-      //    fetch('http://10.1.10.39:5000/api/media/venue/', {
-      //       method: 'POST',
-      //       headers: {
-      //         'Accept': 'application/json',
-      //         'Content-Type': 'application/json'
-      //       },
-      //       body: JSON.stringify({photo: content}),
-      //     });
-      //   })
-
-      // fetch('http://10.1.10.39:5000/api/media/venue/', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Accept': 'application/json',
-      //     'Content-Type': 'application/json'
-      //   },
-      //   body: JSON.stringify({
-      //     name: 'photo'
-      //   })
-      // })
-      // .then(response => response.json());
+      var obj = {
+          uri: path,
+          uploadUrl: 'http://10.8.1.143:5000/api/media/venue/',
+          mimeType: 'image/jpeg',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+      };
+      NativeModules.FileTransfer.upload(obj, (err, res) => {
+          console.log(err);
+      });
     });
   }
 });
